aa60f082f5835e4ef0c15d2670e6083cfd0f4cd3
unknown <exodo302.richy@gmail.com>
2023-03-23 13:15:22 -0600

    Terminado

--- Cannon Ball/Assets/Scripts/Ui.cs
+++ Cannon Ball/Assets/Scripts/Ui.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ui : MonoBehaviour
 {
     public GameObject panelGame;
+    public GameObject gamePanel;
+    public GameObject settingsPanel;
     void Start()
     {
         Time.timeScale = 0;
@@ -20,5 +23,25 @@ public class Ui : MonoBehaviour
         Time.timeScale = 1;
         panelGame.SetActive(false);
         GameManager.obj.gameReady = true;
+        gamePanel.SetActive(true);
+    }
+    public void OptionsGame()
+    {
+        gamePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+
+    }
+    public void CloseOptions()
+    {
+        gamePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
